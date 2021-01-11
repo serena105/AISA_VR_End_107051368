ee273feae3632cdf6dec7388d6d2994c1d0319c7
serena105 <71679914+serena105@users.noreply.github.com>
2021-01-12 07:15:52 +0800

    1/12-07:15

---
+++ VR_End_107051368/Assets/Script/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("VR期末");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ VR_End_107051368/Assets/Script/ScoreManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public Text textScore;
+    public int score;
+    public int Scorein = 2;
+    public AudioClip soundIn;
+
+    private AudioSource aud;
+
+    private void Awake()
+    {
+        aud = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "斧頭")
+        {
+            AddScore();
+        }
+
+        if (other.transform.root.name == "Player")
+        {
+            Scorein = 3;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform.name == "Player")
+        {
+            Scorein = 2;
+        }
+    }
+
+    private void AddScore()
+    {
+        score += Scorein;
+        textScore.text = "Score" + score;
+        aud.PlayOneShot(soundIn, Random.Range(1f, 2f));
+    }
+
+}
